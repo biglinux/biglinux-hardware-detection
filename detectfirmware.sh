9a198b7030e3e8ae323af4cd8802cb0474405f83
Bruno Goncalves <bigbruno@gmail.com>
2022-03-02 15:17:43 -0300

    First Beta Release

---
+++ detectfirmware.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##############
+# Detect firmware needed
+# By Bruno Goncalves < www.biglinux.com.br >
+# 2022/03/01
+# License GPL V2 or greater
+###############
+
+OIFS=$IFS
+IFS=$'\n'
+
+    FIRMWARE_LIST="$(grep -R / firmware/)"
+
+    for i  in  $(dmesg | grep firmware | grep error | grep loading | cut -f2 -d] | cut -f2,5 -d" "); do
+
+        FILE="$(echo "$i" | cut -f2 -d" " | sed 's|.*/||g')"
+
+            for x  in  $(echo "$FIRMWARE_LIST" | grep "$FILE"); do
+
+                    MODULE="$(echo "$i" | cut -f1 -d" ")"
+                    PKG="$(echo "$i" | cut -f1 -d: | cut -f2 -d/)"
+
+                    echo "Firmware: $FILE"
+                    echo "Module: $MODULE"
+                    echo "Package: $PKG"
+
+            done
+
+    done
+
+IFS=$OIFS
